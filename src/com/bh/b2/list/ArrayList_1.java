b55771f4d4e420bb967645c2ef3838765e9a0ef7
zerofour4 <87287030+zerofour4@users.noreply.github.com>
2021-07-28 11:05:59 +0900

    a

--- src/com/bh/b2/list/ArrayList_1.java
+++ src/com/bh/b2/list/ArrayList_1.java
@@ -41,6 +41,19 @@ public class ArrayList_1 {
 		ar.add(true); //auto boxing
 		
 		System.out.println("------");
+		System.out.println(ar.get(0));
+		
+		System.out.println("------------------");
+		ar.add(1, "add"); //1번째 인덱스에 "add" 삽입
+		//나머지 애들 한칸씩 밀려나
+		
+		ar.set(0, 1);
+		ar.remove(0);
+		ar.clear();
+		System.out.println(ar.size());
+		System.out.println(ar.isEmpty()); //boolean타입 비어져있냐없냐 판단
+		
+		
 		for(int i=0;i<ar.size();i++) {
 		System.out.println(ar.get(i));
 		}
